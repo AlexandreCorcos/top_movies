13475176b170974159d1ada501994d9d92571a7e
Alexandre Corcos <acorcos@gmail.com>
2024-03-07 13:54:39 +0000

    finished

--- main.py
+++ main.py
@@ -8,40 +8,32 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-TMDB_KEY = "fecc0b755f3d85a7b706fb5933b267ec"
 
-TMDB_URL = "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1"
-
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZWNjMGI3NTVmM2Q4NWE3YjcwNmZiNTkzM2IyNjdlYyIsInN1YiI6IjY1ZTlhNmFlNmJlYWVhMDE2Mzc5MmE1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fkJ2frv2mVOj0qB5M_0BiBlZ_7HJJx22NDDi5m2wsHM"
-}
-
+MOVIE_DB_API_KEY = ""
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
-
 # CREATE DB
 class Base(DeclarativeBase):
     pass
-
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-
 
 # CREATE TABLE
 class Movie(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     year: Mapped[int] = mapped_column(Integer, nullable=False)
-    description: Mapped[str] = mapped_column(String(250), nullable=False)
-    rating: Mapped[float] = mapped_column(Float, nullable=False)
-    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
-    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
@@ -49,47 +41,58 @@ with app.app_context():
     db.create_all()
 
 
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-#
-# second_movie = Movie(
-#     title="Avatar The Way of Water",
-#     year=2022,
-#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-#     rating=7.3,
-#     ranking=9,
-#     review="I liked the water.",
-#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-# )
-#
-# with app.app_context():
-#     db.session.add(second_movie)
-#     db.session.commit()
-
-class RateMovieForm(FlaskForm):
-    rating = StringField("Your Rating out of 10 e.g. 6.7")
-    review = StringField("Your review")
-    submit = SubmitField("Done")
-
-
-# New Find movie Form
 class FindMovieForm(FlaskForm):
     title = StringField("Movie Title", validators=[DataRequired()])
-    submit = SubmitField("Add movie")
+    submit = SubmitField("Add Movie")
+
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
 
 
 @app.route("/")
 def home():
-    result = db.session.execute(db.select(Movie))
-    all_movies = result.scalars()
+    result = db.session.execute(db.select(Movie).order_by(Movie.rating))
+    all_movies = result.scalars().all()  # convert ScalarResult to Python List
+
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
+
     return render_template("index.html", movies=all_movies)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = FindMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={
+                                "api_key": MOVIE_DB_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={
+                                "api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("rate_movie", id=new_movie.id))
 
 
 @app.route("/edit", methods=["GET", "POST"])
@@ -101,21 +104,8 @@ def rate_movie():
         movie.rating = float(form.rating.data)
         movie.review = form.review.data
         db.session.commit()
-        return redirect(url_for("home"))
+        return redirect(url_for('home'))
     return render_template("edit.html", movie=movie, form=form)
-
-
-@app.route("/add", methods=["GET", "POST"])
-def add_movie():
-    form = FindMovieForm()
-
-    if form.validate_on_submit():
-        movie_title = form.title.data
-        response = requests.get(TMDB_URL, params={"api_key": TMDB_KEY, "query": movie_title})
-        data = response.json()["results"]
-        return render_template("select.html", options=data)
-
-    return render_template("add.html", form=form)
 
 
 @app.route("/delete")
